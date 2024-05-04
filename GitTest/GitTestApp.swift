2f0c75958dacfb3ecab7c0cda1bc70a4921bc06b
TLtlbo <js1436kt@gmail.com>
2024-05-04 23:18:33 +0900

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by 박진성 on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
