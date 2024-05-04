0e05534155b408a1d48234a0962e9f9b36a525af
TLtlbo <js1436kt@gmail.com>
2024-05-04 23:33:03 +0900

    TEST

---
+++ GitTest/SecondView.swift
@@ -0,0 +1,18 @@
+//
+//  SecondView.swift
+//  GitTest
+//
+//  Created by 박진성 on 5/4/24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SecondView()
+}
