d1ae1dc2433a653104eabef4109f116f3a1745f3
TLtlbo <js1436kt@gmail.com>
2024-05-04 23:29:59 +0900

    Second Branch Create

---
+++ GitTest/CheckView.swift
@@ -0,0 +1,18 @@
+//
+//  CheckView.swift
+//  GitTest
+//
+//  Created by 박진성 on 5/4/24.
+//
+
+import SwiftUI
+
+struct CheckView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckView()
+}
